41ee589cbb29aa06ba5e4a3f46b1f4de968637be
irene <ireneptc87@gmail.com>
2022-02-24 11:12:44 -0300

    Mentoria e Curso

--- src/main/java/br/com/dio/desafio/dominio/Main.java
+++ src/main/java/br/com/dio/desafio/dominio/Main.java
@@ -1,4 +1,27 @@
 package br.com.dio.desafio.dominio;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargahoraria(4);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso js");
+        curso2.setDescricao("descricao curso js");
+        curso2.setCargahoraria(6);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição Mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+    }
 }
