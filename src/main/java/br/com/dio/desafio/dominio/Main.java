bfb5199f5e475f8b34a1c83c2c9f68aa7fa29553
irene <ireneptc87@gmail.com>
2022-02-24 22:36:24 -0300

    implementação bootcamp e dev

--- src/main/java/br/com/dio/desafio/dominio/Main.java
+++ src/main/java/br/com/dio/desafio/dominio/Main.java
@@ -1,6 +1,7 @@
 package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,11 +18,41 @@ public class Main {
         System.out.println(curso1);
         System.out.println(curso2);
 
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição Mentoria de Java");
         mentoria.setData(LocalDate.now());
 
         System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devnome = new Dev();
+        devnome.setNome("Irene");
+        devnome.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos: " + devnome.getConteudosInscritos());
+        devnome.progredir();
+        devnome.progredir();
+        devnome.progredir();
+        System.out.println("-------");
+        System.out.println("Conteudos Inscritos: " + devnome.getConteudosInscritos());
+        System.out.println("Conteudos Concluidoc: " + devnome.getConteudosConcluidos());
+
+
+        Dev devnome2 = new Dev();
+        devnome2.setNome("Paulo");
+        devnome2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos: " + devnome2.getConteudosInscritos());
+        devnome2.progredir();
+        System.out.println("-------");
+        System.out.println("Conteudos Inscritos: " + devnome2.getConteudosInscritos());
+        System.out.println("Conteudos Concluidoc: " + devnome2.getConteudosConcluidos());
+
     }
 }
